c004e5914a91594632eae175eb5b5c0974620c50
Karla Nieves <karlaxnieves@gmail.com>
2021-07-14 19:25:36 -0700

    findSteps done

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -24,7 +24,7 @@ function find() { // EXERCISE A
  .groupBy('sc.scheme_id')
 }
 
-function findById(scheme_id) { // EXERCISE B
+async function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -90,9 +90,32 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+      const rows = await db('scheme as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .where('sr.scheme_id', scheme_id)
+      .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
+      .orderBy('st.step_number')
+
+      const results = {
+        scheme_id: rows[0].scheme_id,
+        scheme_name: rows[0].scheme_name,
+        steps: []
+      }
+
+      rows.forEach(row =>{
+        if (row.step_id) {
+          results.steps.push({
+            step_id: row.step_id,
+            step_number: row.step_number,
+            instructions: row.instructions,
+          })
+        }
+      })
+
+      return results 
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -113,6 +136,15 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+
+const rows = await db('schemes as sc')
+.leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+.select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_id')
+.where('sc.scheme_id', scheme_id)
+.orderBy('step_number')
+
+if (!rows[0].step_id) return []
+return rows
 }
 
 function add(scheme) { // EXERCISE D
